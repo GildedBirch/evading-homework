1ab44a784cb4b3471fd6eb262ceb381922a935b2
GildedBirch <thebirchbreaker@gmail.com>
2025-03-14 22:53:07 +0200

    Button Textures

--- scenes/button3d/button_3d.gd
+++ scenes/button3d/button_3d.gd
@@ -4,13 +4,20 @@ extends Area3D
 
 signal pressed
 
-@export var press_depth: float = 0.025
+@export var press_depth: float = 0.05
 @export var button_mesh: Node3D
+
+var button_mesh_pos: Vector3
+
+
+func _ready() -> void:
+	if button_mesh:
+		button_mesh_pos = button_mesh.position
 
 
 func interact() -> void:
 	pressed.emit()
 	# Play sound
 	var tween: Tween = get_tree().create_tween()
-	tween.tween_property(button_mesh, "position:y", position.y - press_depth, 0.2)
-	tween.tween_property(button_mesh, "position:y", position.y + press_depth, 0.2)
+	tween.tween_property(button_mesh, "position:y", button_mesh_pos.y - press_depth, 0.2)
+	tween.tween_property(button_mesh, "position:y", button_mesh_pos.y, 0.2)
